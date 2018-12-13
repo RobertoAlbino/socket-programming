a3023d7649bd5d258b64e3c992772d0616fffda2
Roberto Albino Junior <junior_wash@hotmail.com>
2018-12-12 22:05:03 -0200

    Initial body

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void main() {
-    printf("Hello, World!\n");
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main() {
+	int network_socket = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	if (connection_status == -1) {
+        printf("Aconteceu um problema durante a criação da conexão...\n");
+	}
+	char server_response[256];
+    recv(network_socket, &server_response, sizeof(server_response), 0);
+	printf("Response data: %s", server_response);
+	return 0;
 }
+
